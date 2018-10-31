7c69c68511fc0ae1e3243f118142c08a1e758d6c
Eric <elymberopoulos@luc.edu>
2018-10-31 09:14:55 -0500

    Moved javascript to external JS files to clean up the main.html page. Also got navigator Geolocate working so map centers on a users current location.

--- map.js
+++ map.js
@@ -1,28 +1,30 @@
-function initMap() {
-  myMap = new google.maps.Map(document.getElementById('mainMap'), {
-    center: new google.maps.LatLng(41.881832, -87.623177),
-    zoom: 7
-  });
-  mainWindow = new google.maps.mainWindow;
+function getLocation() {
+  var lat;
+  var lng;
+  var getLocation = navigator.geolocation.getCurrentPosition(function(position) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        lat = position.coords.latitude;
+        lng = position.coords.longitude;
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var thisPosition = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
+        var latlong = new google.maps.LatLng(lat, lng);
+        var mapOptions = {
+          center: latlong,
+          zoom: 13,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("mainMap"), mapOptions);
+        var marker = new google.maps.Marker({
+          position: latlong,
+          map: mainMap});
 
-      myMap.setPosition(thisPosition);
-      mainWindow.setContent('Location found.');
-      mainWindow.open(myMap);
-      myMap.setCenter(thisPosition);
-    }, function() {
-      handleLocationError(true, mainWindow, myMap.getCenter());
-    });
-  } else {
-
-    handleLocationError(false, mainWindow, myMap.getCenter());
-  }
-
+      }, function() {
+        handleLocationError(true, infoWindow, map.getCenter());
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  })
 }
-initMap();
+getLocation();
